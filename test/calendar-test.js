6320ac584b54459c4ae9f8c4a7c576511b4ac247
Christos Vontas <christos@redsift.io>
2016-07-18 14:35:46 +0100

    Fixed tests

--- test/calendar-test.js
+++ test/calendar-test.js
@@ -2,7 +2,7 @@ var tape = require("@redsift/tape-reel")("<div id='test'></div>");
     d3 = require("d3-selection"),
     calendar = require("../");
 //     svg = require("@redsift/d3-rs-svg"),
-tape("html() generates and updates svg", function(t) {
+tape("html() generates sth with no data", function(t) {
     var host = calendar.html();
     var el = d3.select('#test');
 
@@ -13,26 +13,32 @@ tape("html() generates and updates svg", function(t) {
     // empty datum
     el.datum([]).call(host);
     t.equal(el.selectAll('svg').size(), 1);
+    
+    t.end();
+});
 
+tape("html() testing parameters", function(t) {
+    var host = calendar.html();
+    var el = d3.select('#test');
     //last weeks
     var lw = 12;
     host = calendar.html().lastWeeks(lw);
     el.datum([]).call(host);
     t.equal(el.selectAll('svg').size(), 1);
-    t.equal(el.selectAll('.svg-child > g').size(), lw+1);
+    t.equal(el.selectAll('.svg-svg > g').size(), lw+1);
 
     //next weeks
     var nw = 12;
     host = calendar.html().nextWeeks(nw);
     el.datum([]).call(host);
     t.equal(el.selectAll('svg').size(), 1);
-    t.equal(el.selectAll('.svg-child > g').size(), nw+1);
+    t.equal(el.selectAll('.svg-svg > g').size(), nw+1);
 
     //both next and last weeks
     host = calendar.html().lastWeeks(lw).nextWeeks(nw);
     el.datum([]).call(host);
     t.equal(el.selectAll('svg').size(), 1);
-    t.equal(el.selectAll('.svg-child > g').size(), lw+nw+1);
+    t.equal(el.selectAll('.svg-svg > g').size(), lw+nw+1);
 
     // with some data
     var data = [ {date: '2016-05-01', value: 10},
@@ -42,7 +48,7 @@ tape("html() generates and updates svg", function(t) {
     host = calendar.html().lastWeeks(lw);
     el.datum(data).call(host);
     t.equal(el.selectAll('svg').size(), 1);
-    t.equal(el.selectAll('.svg-child > g').size(), lw+1);
+    t.equal(el.selectAll('.svg-svg > g').size(), lw+1);
     
     t.end();
-});
+})
